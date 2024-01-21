3f82f19f42b7345559f4fa04e36895f483d88048
Atul sahani <atulsahani8888@gmail.com>
2024-01-21 13:25:59 +0530

    post and user back to normal

--- routes/post.js
+++ routes/post.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
 
-// ... (rest of your code)
+    mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
 
-const postSchema = mongoose.Schema({
-  picture: String,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user"
-  },
-  caption: String,
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user"
-    }
-  ]
-});
+    const postSchema = mongoose.Schema({
+        picture: String,
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
+        },
+        caption: String,
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "user"
+            }
+        ]
+    });
 
-module.exports = mongoose.model("post", postSchema);
+
+    module.exports = mongoose.model("post", postSchema);
